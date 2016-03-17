431708b47fca9c2cb2a133cd8ffe036e4135e0f3
Brian M. Sutin <github@skewray.com>
2016-03-17 14:21:11 -0700

    correcting 11D rotation error

--- NewSegm.m
+++ NewSegm.m
@@ -111,6 +111,7 @@ function [] = NewSegm(GeneratePlots,debugParameters);
 % Modified: Brian Sutin, 2016-03-17
 %     Clarified documentation on the 't' rotation in sectiona 11A and 12
 %     Added extra three decimal places to last four columns of sections 11A-D
+%     Corrected incorrect rotation direction for 11D
 %
 %######################################## set master parameters ########################################
 tstart=cputime;
@@ -613,13 +614,13 @@ for i=1:n_segments
     
     m = 1;
     R11 = cos(m*angle);
-    R21 = -sin(m*angle);
+    R21 = sin(m*angle);
     PSABWZernike(i).c31 = R11 * BWZernike(i).c31;
     PSABWZernike(i).c3m1 = R21 * BWZernike(i).c31;  
     
     m = 2;
     R11 = cos(m*angle);
-    R21 = -sin(m*angle);
+    R21 = sin(m*angle);
     PSABWZernike(i).c22 = R11 * BWZernike(i).c22;
     PSABWZernike(i).c2m2 = R21 * BWZernike(i).c22;  
     PSABWZernike(i).c42 = R11 * BWZernike(i).c42;
@@ -627,7 +628,7 @@ for i=1:n_segments
     
     m = 3;
     R11 = cos(m*angle);
-    R21 = -sin(m*angle);
+    R21 = sin(m*angle);
     PSABWZernike(i).c33 = R11 * BWZernike(i).c33;
     PSABWZernike(i).c3m3 = R21 * BWZernike(i).c33;  
     
@@ -1465,7 +1466,7 @@ fprintf(fid,' \n');
 fprintf(fid,'-----------------------------------------------  SECTION 11: OFF-AXIS PRESCRIPTIONS -------------------------------------------------------\n');
 fprintf(fid,'NOTES: * all coefficients are listed in MICRONS\n');
 fprintf(fid,'       * all coefficients are calculated based on a nominal segment radius as listed below (in meters)\n');
-fprintf(fid,'       * The angle t (in degrees) is the angle measured in the plane XY_PSA from the X_PSA axis to the projection into XY_PSA of the R_M1 line from O_M1 to the projection of O_PSA in the XY_M1 plane; this makes -t the rotation from SCRS(k) to PSACRS(k).\n');
+fprintf(fid,'       * The angle t (in degrees) is the angle measured in the plane XY_PSA from the X_PSA axis to the projection into XY_PSA of the R_M1 line from O_M1 to the projection of O_PSA in the XY_M1 plane; this makes t the rotation from SCRS(k) to PSACRS(k).\n');
 fprintf(fid,'       * Zernike coefficients are based on the 7 monomial coefficients listed below (i.e. a20, a22, a31, a33, a40, a42, and a44); all other monomial coefficients are assumed equal to zero\n');
 fprintf(fid,'       * Zernike coefficients are identified using the n,m notation, i.e. C20=focus, C22=astigmatism, C31=coma, C33=trefoil, C40=spherical aberration, C42=higher order astigmatism\n');
 fprintf(fid,'11A: CYLINDRICAL MONOMIAL COEFFICIENTS (microns)\n');
